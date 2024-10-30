d51185e857a1f1ea2324399850a62abe984d3172
Otaiki1 <otaikisadiq2804@gmail.com>
2024-10-30 17:19:00 +0100

    feat: add connect wallet functionality

--- frontend/src/app/components/Addressbar.tsx
+++ frontend/src/app/components/Addressbar.tsx
@@ -1,41 +1,37 @@
 "use client";
 import {
-	useAccount,
-	useDisconnect,
-	useStarkProfile,
+    useAccount,
+    useDisconnect,
+    useStarkProfile,
 } from "@starknet-react/core";
 import Image from "next/image";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import GenericModal from "./GenericModal";
 
-const AddressBar = ({
-	setOpenConnectedModal,
-}: {
-	setOpenConnectedModal: Dispatch<SetStateAction<boolean>>;
-}) => {
-	const { address } = useAccount();
-	const { data: starkProfile } = useStarkProfile({
-		address,
-	});
+const AddressBar = () => {
+    const { address } = useAccount();
+    const { data: starkProfile } = useStarkProfile({
+        address,
+    });
 
-	const toggleModal = () => {
-		setOpenConnectedModal((prev) => !prev);
-	};
+    const { disconnect } = useDisconnect();
 
-	if (!address) {
-		return null;
-	}
+    if (!address) {
+        return null;
+    }
 
-	return (
-		<button
-			onClick={toggleModal}
-			className="bg-primary py-2 px-4 text-white rounded-full transition duration-300"
-		>
-			<span>
-				{address?.slice(0, 6).concat("...").concat(address?.slice(-5))}
-			</span>
-		</button>
-	);
+    return (
+        <button
+            onClick={() => disconnect()}
+            className="w-[12.5rem] h-14 grid py-2 px-[1.25rem]
+          place-items-center rounded-2xl gap-2 text-textPrimary
+          font-medium text-base bg-transparent font-roboto border-gray-500 border"
+        >
+            <span>
+                {address?.slice(0, 6).concat("...").concat(address?.slice(-5))}
+            </span>
+        </button>
+    );
 };
 
 export default AddressBar;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,43 +1,19 @@
 "use client";
-import {
-  DiscordIcon,
-  LearnmoreIcon,
-  MenuIcon,
-  ScanIcon,
-  TelegramIcon,
-  TwitterIcon,
-} from "../assets/icons";
-import Link from "next/link";
-import { useEffect, useMemo, useState } from "react";
-import Modal from "./Modal";
-import { useContractWrite, useWaitForTransaction } from "@starknet-react/core";
-import { useAccount } from "@starknet-react/core";
-import { useParams } from "next/navigation";
-import AddressBar from "@/components/Addressbar";
-import ConnectModal from "@/components/ConnectModal";
-import NavBar from "./components/NavBar";
+
 import HeroSection from "./components/HeroSection";
 import ContentSection from "./components/ContentSection";
-import Footer from "./components/Footer";
 
 export default function Home() {
-  const { address } = useAccount();
-  const [openConnectedModal, setOpenConnectedModal] = useState(false);
-
-  const toggleUserModal = () => {
-    setOpenConnectedModal((prev) => !prev);
-  };
-
-  return (
-    <main>
-      <div>
-        <header className="container mx-auto">
-          <HeroSection />
-        </header>
-      </div>
-      <main className="container mx-auto">
-        <ContentSection />
-      </main>
-    </main>
-  );
+    return (
+        <main>
+            <div>
+                <header className="container mx-auto">
+                    <HeroSection />
+                </header>
+            </div>
+            <main className="container mx-auto">
+                <ContentSection />
+            </main>
+        </main>
+    );
 }
