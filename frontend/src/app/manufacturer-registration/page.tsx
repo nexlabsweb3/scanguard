28fcc5e8641b159ae79731329288e17d8789bcdd
KevinLatino <latinokevin9@gmail.com>
2024-11-23 01:56:56 -0600

    Adding bg-image

--- frontend/src/app/manufacturer-registration/page.tsx
+++
@@ -1,9 +0,0 @@
-import ManufacturerForm from "../components/ManufacturerForm";
-
-export default function ManufacturerPage() {
-    return (
-        <div className="min-h-screen mb-20 flex flex-col justify-center items-center bg-[##1E1E1E]">
-            <ManufacturerForm />
-        </div>
-    );
-}
