f39a3004a7824ca12d7d7be47f285713941d38d7
Jethrolopwus <lopwusjethro92@gmail.com>
2024-11-30 08:17:57 +0100

    UPDATE: create a layout file for proper file and folder structure

---
+++ frontend/src/app/Home/scan/page.tsx
@@ -0,0 +1,13 @@
+import ScanPage from '@/app/scan/[product]/page';
+import ScanButton from '@/components/ScanButton';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <ScanPage />
+    </div>
+  );
+};
+
+export default page;
